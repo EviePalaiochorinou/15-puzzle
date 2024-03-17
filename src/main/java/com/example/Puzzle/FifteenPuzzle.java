b3b0b9a21ea1284e442a90a834072db041f79961
Evie <evangeliapal@gmail.com>
2024-03-17 11:54:12 +0100

    Initial commit.

---
+++ src/main/java/com/example/Puzzle/FifteenPuzzle.java
@@ -0,0 +1,13 @@
+package com.example.Puzzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifteenPuzzle {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifteenPuzzle.class, args);
+	}
+
+}
